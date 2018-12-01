ad7607e4d6d77b47e9e94dd23913d65dbe541fb1
kchro <jeffhara@mercari.com>
2018-12-01 15:38:45 -0800

    added docs

--- query_segmenter/unsupervised.py
+++ query_segmenter/unsupervised.py
@@ -27,13 +27,29 @@ class Segmenter(object):
     def __init__(self):
         """Construct Segmenter."""
         self.scores = defaultdict(float)
-        self.stats = {}
+        self.stats = defaultdict(lambda: self._initialize_ngram())
 
-    def compute_scores(self, queries, alpha=0):
+    def compute_scores(self, queries, alpha=1, beta=0.0):
         """Compute segmentation scores.
 
-        Algorithm:
+        Parameters
+        ----------
+        queries : list (n_queries)
+            Training data for determining segments.
+        alpha : int, optional
+            Minimum query frequency. All words in an n-gram must occur in
+            at least alpha queries to be considered a "significant n-gram."
+        beta : float, optional
+            beta * k is the segmentation score threshold, where k is the number
+            of queries that contains all the words of a given n-gram.
 
+        Returns
+        -------
+        self.scores : dict
+            A mapping of ngram to segmentation score.
+
+        Algorithm
+        ---------
         Consider an n-gram M = [word_1, word_2, ..., word_n].
 
         Let {query_1, query_2, ..., query_k} be the subset of queries
@@ -47,18 +63,37 @@ class Segmenter(object):
         self._precompute_stats(sig_ngrams, queries)
 
         for ngram in sig_ngrams:
-            frequency = self.stats[ngram]['frequency']
-            co_occur = self.stats[ngram]['co_occur']
-            expectation = self.stats[ngram]['expectation']
+            N = self.stats[ngram]['frequency']
+            k = self.stats[ngram]['co_occur']
+            E_X = self.stats[ngram]['expectation']
 
-            score = (frequency - expectation)**2 / co_occur
+            score = 2 * (N - E_X)**2 / k
+
+            if score < beta * k:
+                # if the score does not exceed threshold
+                continue
+
             self.scores[ngram] = score
 
         return self.scores
 
     def segment(self, query):
-        """Segment a new query."""
-        pass
+        """Segment a query (single or batch).
+
+        Parameters
+        ----------
+        query : str or array-like
+
+        Returns
+        -------
+        segments : 1 or 2D list of strings
+
+        """
+        if isinstance(query, str):
+            segments = self._segment(query)
+        elif hasattr(query, '__iter__'):
+            segments = list(map(self._segment, query))
+        return segments
 
     def _get_significant_ngrams(self, queries, ngram_range=(2, 10), alpha=1):
         """Get significant n-grams in the list of queries.
@@ -89,9 +124,6 @@ class Segmenter(object):
 
         """
         for ngram in sig_ngrams:
-            if ngram not in self.stats:
-                self._initialize_ngram(ngram)
-
             ngram_len = len(ngram.split())
             for query in queries:
                 sub_queries = query.split()
@@ -109,9 +141,9 @@ class Segmenter(object):
 
         return self.stats
 
-    def _initialize_ngram(self, ngram):
+    def _initialize_ngram(self):
         """Initialize the information to collect about each ngram."""
-        self.stats[ngram] = {
+        return {
             'frequency': 0,
             'co_occur': 0,
             'expectation': 0
@@ -120,3 +152,32 @@ class Segmenter(object):
     def _match_ngram(self, ngram, query):
         match = re.search(rf'\b{ngram}\b', query)
         return match is not None
+
+    def _segment(self, query):
+        """Segment a query with dynamic programming."""
+        query = query.split()
+        query_len = len(query)
+        val = [0] * (query_len+1)
+        idx = [0] * (query_len+1)
+        for i in range(1, query_len+1):
+            max_val = -1
+            for j in range(i):
+                subquery = ' '.join(query[j:i])
+
+                if subquery in self.scores:
+                    score = self.scores[subquery]+val[j]
+                else:
+                    score = val[j]
+
+                if score >= max_val:
+                    max_val = score
+                    idx[i] = j
+            val[i] = max_val
+
+        segments = []
+        while query_len > 0:
+            query_len = idx[query_len]
+            segments.insert(0, ' '.join(query[query_len:]))
+            query = query[:query_len]
+
+        return segments
